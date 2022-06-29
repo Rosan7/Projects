2b4d08541d23e4771ebaf1a0a6ec4fa6e103db1d
Rosan7 <85017693+Rosan7@users.noreply.github.com>
2022-06-29 21:58:32 +0530

    Update main.py

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -6,10 +6,19 @@ my_menu = Menu()
 my_money_machine = MoneyMachine()
 my_coffee_maker = CoffeeMaker()
 print("Welcome to Coffee Mahal . We are happy to have you as our customer")
-while True:
 
-    menuu = my_menu.get_items()
-    customer_demand = input("What would you like to have ? " + menuu + "?")
+is_on = True
 
-    my_money_machine.report()
-    my_coffee_maker.report()
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
